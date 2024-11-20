023873be301fe6df9c0806ec2729f255096627ed
jay <emma.jordan@bsu.edu>
2024-11-19 20:23:51 -0500

    creation of necessary pages

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,13 @@
+import { PrismaClient } from "@prisma/client"
 
+const prisma = new PrismaClient()
 
-export default function Home() {
-  return (
-    <>
-</>
-  );
+export default async function Home() {
+    const recipie = await prisma.recipie.findMany();
+    const displayRecipie = recipie.map((recipie, index) => <li key  = {index}>{recipie.steps}</li>)
+return (
+    <main>
+    {displayRecipie}
+    </main>
+);
 }
